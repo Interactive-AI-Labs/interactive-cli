50edf251b4d217f5d6fdd801311ea87357d546ff
Stefan Bullones <sbullones@interactive.ai>
2026-01-26 14:18:17 +0100

    Bump: 0.1.0 → 0.1.1

--- cmd/root.go
+++ cmd/root.go
@@ -9,7 +9,7 @@ import (
 )
 
 const (
-	version            = "0.1.0"
+	version            = "0.1.1"
 	cfgDirName         = ".interactiveai"
 	sessionFileName    = "session_cookies.json"
 	defaultHTTPTimeout = 15 * time.Second
