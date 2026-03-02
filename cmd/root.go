69d631a13ec84bf25172fd7fc620840457913671
Stefan Bullones <sbullones@interactive.ai>
2026-03-02 16:29:07 +0100

    Bump: 0.6.0 → 0.7.0

--- cmd/root.go
+++ cmd/root.go
@@ -9,7 +9,7 @@ import (
 )
 
 const (
-	version            = "0.6.0"
+	version            = "0.7.0"
 	cfgDirName         = ".interactiveai"
 	sessionFileName    = "session_cookies.json"
 	defaultHTTPTimeout = 15 * time.Second
