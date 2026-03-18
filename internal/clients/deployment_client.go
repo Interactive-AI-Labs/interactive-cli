65723f0ad6639836876d40bec50efef5f157f90a
ajaen-ai <ajaen@interactive.ai>
2026-03-18 16:42:38 +0100

    Avoid showing long errors when client cancels the request

--- internal/clients/deployment_client.go
+++ internal/clients/deployment_client.go
@@ -122,7 +122,11 @@ func (c *DeploymentClient) do(req *http.Request) (*http.Response, error) {
 	if err := ApplyAuth(req, c.apiKey, c.cookies); err != nil {
 		return nil, err
 	}
-	return c.httpClient.Do(req)
+	resp, err := c.httpClient.Do(req)
+	if err != nil && req.Context().Err() != nil {
+		return nil, req.Context().Err()
+	}
+	return resp, err
 }
 
 func (c *DeploymentClient) newRequest(
