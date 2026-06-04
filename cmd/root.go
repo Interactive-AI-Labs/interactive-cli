9aea4d24244c0bf5e7423f56e319722bfd894195
pedro palacio estrada <ppalacio@interactive.ai>
2026-06-04 15:51:21 +0200

    Bump: 0.33.0 → 0.34.0

--- cmd/root.go
+++ cmd/root.go
@@ -11,7 +11,7 @@ import (
 )
 
 const (
-	version            = "0.33.0"
+	version            = "0.34.0"
 	cfgDirName         = ".interactiveai"
 	sessionFileName    = "session_cookies.json"
 	defaultHTTPTimeout = 30 * time.Second
