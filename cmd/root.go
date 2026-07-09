a08aebe4070100988076788368685b50f96c5f12
ajaen-ai <ajaen@interactive.ai>
2026-07-09 16:48:05 +0200

    Bump: 0.35.2 → 0.36.0

--- cmd/root.go
+++ cmd/root.go
@@ -12,7 +12,7 @@ import (
 )
 
 const (
-	version            = "0.35.2"
+	version            = "0.36.0"
 	cfgDirName         = ".interactiveai"
 	sessionFileName    = "session_cookies.json"
 	defaultHTTPTimeout = 30 * time.Second
