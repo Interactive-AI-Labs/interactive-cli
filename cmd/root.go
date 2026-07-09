0028b53dddbe50378068c7d657627365bfce3bd9
dgallego-iai <dgallego@interactive.ai>
2026-07-09 17:33:34 +0200

    Bump: 0.37.0 → 0.37.1

--- cmd/root.go
+++ cmd/root.go
@@ -12,7 +12,7 @@ import (
 )
 
 const (
-	version            = "0.37.0"
+	version            = "0.37.1"
 	cfgDirName         = ".interactiveai"
 	sessionFileName    = "session_cookies.json"
 	defaultHTTPTimeout = 30 * time.Second
