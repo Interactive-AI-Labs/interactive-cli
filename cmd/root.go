608a8e143dfd0363065519e055680cadaf80ca6b
Gabriel <gandrade@interactive.ai>
2026-07-16 17:07:40 +0200

    Bump: 0.37.5 → 0.38.0

--- cmd/root.go
+++ cmd/root.go
@@ -12,7 +12,7 @@ import (
 )
 
 const (
-	version            = "0.37.5"
+	version            = "0.38.0"
 	cfgDirName         = ".interactiveai"
 	sessionFileName    = "session_cookies.json"
 	defaultHTTPTimeout = 30 * time.Second
